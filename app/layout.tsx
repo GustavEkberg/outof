d14384f00fc29f0df3c20e1a232085c94df2dccd
GustavEkberg <krypteratkadaver@gmail.com>
2026-03-02 09:46:30 +0100

    chore: wording

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Outof',
+  title: 'outof',
   description: 'Minimal grocery list service for Telegram'
 };
 
